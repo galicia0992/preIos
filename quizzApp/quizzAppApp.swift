2a0223f978ac8f2c4ee02c88b8cb6f43630a2b5f
galicia0992 <perrako92@live.com.mx>
2025-01-01 23:01:36 -0600

    Initial Commit

---
+++ quizzApp/quizzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizzAppApp.swift
+//  quizzApp
+//
+//  Created by Yair on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct quizzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quizzAppTests/quizzAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  quizzAppTests.swift
+//  quizzAppTests
+//
+//  Created by Yair on 01/01/25.
+//
+
+import Testing
+@testable import quizzApp
+
+struct quizzAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
